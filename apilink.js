ead12f8beef93f3b340ff4632b74e4e708eb1969
Jenny Lin <125613441+jennylin2003@users.noreply.github.com>
2023-05-17 00:13:38 -0400

    Create apilink.js

---
+++ apilink.js
@@ -0,0 +1,9 @@
+// API call
+fetch('http://localhost:5103/')
+  .then(response => response.json())
+  .then(data => {
+    // Process the data
+  })
+  .catch(error => {
+    // Handle the error
+  });
